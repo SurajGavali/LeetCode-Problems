276a841f8c985e1d47ad77de1d3a7273f3d48172
Suraj Gavali <surajgavali@Surajs-MacBook-Pro.local>
2025-06-22 09:12:15 +0530

    solved easy problem longets commmon prefix

---
+++ Easy/Problem_14.java
@@ -0,0 +1,18 @@
+public class Problem_14 {
+
+    public static void main(String[] args) {
+        System.out.println("Longest common prefix :: " + longestCommonPrefix(new String[]{"flower", "flow", "flight"}));
+    }
+
+    public static String longestCommonPrefix(String[] strs) {
+        if (strs == null || strs.length == 0) return "";
+        String prefix = strs[0];
+        for (int i = 1; i < strs.length; i++) {
+            while (!strs[i].startsWith(prefix)) {
+                prefix = prefix.substring(0, prefix.length() - 1);
+                if (prefix.isEmpty()) return "";
+            }
+        }
+        return prefix;
+    }
+}
